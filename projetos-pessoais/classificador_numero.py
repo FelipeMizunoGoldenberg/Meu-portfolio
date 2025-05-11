436f051f0254dcf207ab2e36c2d439cebcf3099b
FELIPE MIZUNO GOLDENBERG <felipe.goldenberg@sempreceub.com>
2025-05-11 19:56:23 -0300

    Create classificador_numero.py

---
+++ projetos-pessoais/classificador_numero.py
@@ -0,0 +1,8 @@
+print("POSITIVO, NULO E NEGATIVO:\n")
+numero = float(input("Digite o Número: "))
+if numero > 0:
+    print("NÚMERO POSITIVO!")
+elif numero == 0:
+    print("NÚMERO NULO!")
+else:
+    print("NÚMERO NEGATIVO!")
